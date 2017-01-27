0f58ee1b5cdd3e8a8c8b0456f734e95818104063
Paul Gestwicki <pvgestwicki@bsu.edu>
2017-01-27 13:32:59 -0500

    Fix output directory cleaning
    Previously this was running rm/**/*, which did not remove
    dot-files. This meant that .gitignore was still there, so when the
    subsequent `git add .` was executed, bower_components was
    ignored.

--- deploy.sh
+++ deploy.sh
@@ -36,7 +36,7 @@ git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
 cd ..
 
 # Clean out existing contents
-rm -rf out/**/* || exit 0
+rm -rf out/* out/.gitignore out/.travis.yml || exit 0
 
 # Run our compile script
 doCompile
@@ -62,7 +62,7 @@ ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
 ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
 ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in deploy_key.enc -out deploy_key -d
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ../deploy_key.enc -out deploy_key -d
 chmod 600 deploy_key
 eval `ssh-agent -s`
 ssh-add deploy_key
