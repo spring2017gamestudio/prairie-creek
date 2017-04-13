973776489a39014bfdfd9df8c51850df0af75244
Paul Gestwicki <pvgestwicki@bsu.edu>
2017-04-13 12:30:02 -0400

    Implement spirit selection
    The right spirit image path is chosen based on the selections at the
    current site, although right now they are all placeholder images.
    
    The text is current fixed based on artificial vs natural, but it should
    probably be bound to the site, to reflect the location and sense used.

---
+++ src/spirit-selector.js
@@ -0,0 +1,13 @@
+function selectSpirit(site) {
+    var sum = 0;
+    for (var i = 0; i < site.responses.length; i++) {
+        if (site.responses[i].selected) {
+            sum += site.responses[i].value;
+        }
+    }
+    var type = (sum < 0) ? 'artificial' : 'natural';
+    return {
+        image: site.shortname + '-' + type + '.png',
+        text: (sum < 0) ? 'You found artificial things' : 'You found natural things'
+    };
+}
